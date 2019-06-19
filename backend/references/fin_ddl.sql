68cc2cd94c9e6ecc90eba773e4262b93a1289a61
kimheegeon <32567051+kimheegeon@users.noreply.github.com>
2019-06-19 17:08:27 +0900

    ddl

---
+++ backend/references/fin_ddl.sql
@@ -0,0 +1,109 @@
+-- 게시물
+DROP TABLE POST;
+
+-- 금융상품상세
+DROP TABLE PRODUCT;
+
+-- 좋아요
+DROP TABLE LIKEs;
+
+-- 계좌 정보
+DROP TABLE ACCOUNT;
+
+-- 유저
+DROP TABLE USER;
+
+-- 게시물
+CREATE TABLE POST (
+	PNO      VARCHAR(255)  NOT NULL, -- 게시물번호
+	UID      VARCHAR(255)  NOT NULL, -- 유저ID
+	contents VARCHAR(5000) NULL      -- 내용
+);
+
+-- 게시물
+ALTER TABLE POST
+	ADD
+		CONSTRAINT PK_POST -- 게시물 기본키
+		PRIMARY KEY (
+			PNO -- 게시물번호
+		);
+
+-- 금융상품상세
+CREATE TABLE PRODUCT (
+	PNAME VARCHAR(1023) NOT NULL, -- 상품명
+	date  DATE          NULL,     -- 상품등록일
+	code  VARCHAR(255)  NULL      -- 상품코드
+);
+
+-- 좋아요
+CREATE TABLE LIKEs (
+	UID      VARCHAR(255)  NOT NULL, -- 유저ID
+	contents VARCHAR(5000) NULL,     -- 내용
+	PNO      VARCHAR(255)  NOT NULL  -- 게시물번호
+);
+
+-- 계좌 정보
+CREATE TABLE ACCOUNT (
+	ANO  VARCHAR(255) NOT NULL, -- 계좌번호
+	bank VARCHAR(255) NULL,     -- 은행
+	UID  VARCHAR(255) NOT NULL  -- 유저ID
+);
+
+-- 유저
+CREATE TABLE USER (
+	UID      VARCHAR(255) NOT NULL, -- 유저ID
+	email    VARCHAR(255) NOT NULL, -- ID
+	password VARCHAR(255) NOT NULL  -- 비밀번호
+);
+
+-- 유저
+ALTER TABLE USER
+	ADD
+		CONSTRAINT PK_USER -- 유저 기본키
+		PRIMARY KEY (
+			UID -- 유저ID
+		);
+
+-- 게시물
+ALTER TABLE POST
+	ADD
+		CONSTRAINT FK_USER_TO_POST -- 유저 -> 게시물
+		FOREIGN KEY (
+			UID -- 유저ID
+		)
+		REFERENCES USER ( -- 유저
+			UID -- 유저ID
+		);
+
+-- 좋아요
+ALTER TABLE LIKEs
+	ADD
+		CONSTRAINT FK_USER_TO_LIKEs -- 유저 -> 좋아요
+		FOREIGN KEY (
+			UID -- 유저ID
+		)
+		REFERENCES USER ( -- 유저
+			UID -- 유저ID
+		);
+
+-- 좋아요
+ALTER TABLE LIKEs
+	ADD
+		CONSTRAINT FK_POST_TO_LIKEs -- 게시물 -> 좋아요
+		FOREIGN KEY (
+			PNO -- 게시물번호
+		)
+		REFERENCES POST ( -- 게시물
+			PNO -- 게시물번호
+		);
+
+-- 계좌 정보
+ALTER TABLE ACCOUNT
+	ADD
+		CONSTRAINT FK_USER_TO_ACCOUNT -- 유저 -> 계좌 정보
+		FOREIGN KEY (
+			UID -- 유저ID
+		)
+		REFERENCES USER ( -- 유저
+			UID -- 유저ID
+		);
