6e000babd1082ff5e1abd1d2bcb586a154df9d76
yalekim <yaleees2@gmail.com>
2019-06-24 19:54:51 +0900

    로그인/회원가입 입력란 미입력시 에러메세지 출력&api송신제한

--- frontend/src/components/Join.vue
+++ frontend/src/components/Join.vue
@@ -1,23 +1,19 @@
 <template>
     <div class="container">
         <label for="uname"><b>이메일</b></label>
-        <input type="text" placeholder="EMail" name="uname" v-model="user_id" required>
+        <input type="text" placeholder="EMail" v-model="mail_address" required>
+        <div v-if="errMailAddress" class="invalid-feedback">{{ errMsgMailAddress }}</div>
 
         <label for="psw"><b>비밀번호</b></label>
-        <input type="password" placeholder="Password" name="psw" v-model="user_pwd" required>
+        <input type="password" placeholder="Password" v-model="password" required>
+        <div v-if="errPassword" class="invalid-feedback">{{ errMsgPassword }}</div>
 
         <label for="nickname"><b>닉네임</b></label>
-        <input type="nickname" placeholder="nickname" name="nickname" v-model="nickname" required>
-
-        <button v-on:click="loginSubmit">회원가입</button>
-        <label>
-            <input type="checkbox" checked="checked" name="remember"> Remember me
-        </label>
+        <input type="nickname" placeholder="Nickname" v-model="nickname" required>
+        <div v-if="errNickName" class="invalid-feedback">{{ errMsgNickName }}</div>
     
-        <div class="container" style="background-color:#f1f1f1">
-            <button type="button" class="cancelbtn">Cancel</button>
-            <span class="psw">Forgot <a href="#">password?</a></span>
-        </div>
+        <button v-on:click="loginSubmit">회원가입</button>
+        <button type="button" class="cancelbtn" v-on:click="moveToLogin">돌아가기</button>
     </div>
 </template>
 <script>
@@ -27,18 +23,27 @@ export default {
   name: 'Join',
   data () {
     return {
-        user_id:"",
-        user_pwd:"",
-        nickname:""
+        mail_address:"",
+        password:"",
+        nickname:"",
+        errMailAddress:"false",
+        errMsgMailAddress:"",
+        errPassword:"false",
+        errMsgPassword:"",
+        errNickName:"false",
+        errMsgNickName:""
     }
   },
   methods:{
     loginSubmit: function() {
       console.log("회원가입")
-      axios.post(`http://127.0.0.1:3000/join`, 
+      if (!this.chkValidation()) {
+         return false
+      } else {
+        axios.post(`http://127.0.0.1:3000/join`, 
         { 
-            email: this.user_id,
-            password: this.user_pwd,
+            email: this.mail_address,
+            password: this.password,
             nickname: this.nickname
         }).then(
             (response) => { 
@@ -47,13 +52,65 @@ export default {
                     this.$router.push("/MainContent")
                 }
             },
-	        (error) => { console.log(error) }
+            (error) => { console.log(error) }
             // res=> {
             //     console.log(res)
             //     this.$router.push("/private")
             // }
         )
+      }
+    },
+    chkValidation: function() {
+        //메일 미입력 체크
+        if (this.mail_address == null || this.mail_address == "") {
+          this.errMailAddress = true
+          this.errMsgMailAddress = "메일주소를 입력해주세요."
+        } else {
+          this.errMailAddress = false
+          this.errMsgMailAddress = ''
+        }
+        //패스워드 미입력 체크
+        if (this.password == null || this.password == '') {
+          this.errPassword = true
+          this.errMsgPassword = "패스워드를 입력해주세요."
+        } else {
+          this.errPassword = false
+          this.errMsgPassword = ''
+        }
+
+        //닉네임 미입력 체크
+        if (this.nickname == null || this.nickname == '') {
+          this.errNickName = true
+          this.errMsgNickName = "닉네임을 입력해주세요."
+        } else {
+          this.errNickName = false
+          this.errMsgNickName = ''
+        }
+        
+        //에러가 없을때만 true를 반환
+        if (this.errMailAddress || this.errPassword || this.errNickName) {
+          return false
+        } else {
+          return true
+        }
+    },
+    moveToLogin: function() {
+        this.$router.push("/")
     }
   }
 }
 </script>
+<style>
+.invalid-feedback {
+  display: block;
+  color: #e50000;
+  font-size: 11px;
+  font-weight: bold;
+  width: 55%;
+  margin-left: 50%;
+}
+label {
+    float: left;
+    width: 55%;
+}
+</style>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="container">
         <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" v-model="user_id" required>
+        <input type="text" placeholder="Enter Username" v-model="mail_address" required>
+        <div v-if="errMailAddress" class="invalid-feedback">{{ errMsgMailAddress }}</div>
 
         <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" v-model="user_pwd" required>
+        <input type="password" placeholder="Enter Password" v-model="password" required>
+        <div v-if="errPassword" class="invalid-feedback">{{ errMsgPassword }}</div>
 
         <button v-on:click="loginSubmit">로그인</button>
-    
-        <div class="container" style="background-color:#f1f1f1">
-            <button type="button" class="cancelbtn">Cancel</button>
-            <button v-on:click=moveToJoin()>회원가입</button>
-        </div>
+        <button v-on:click=moveToJoin()>회원가입</button>
     </div>
 </template>
 <script>
@@ -21,29 +19,76 @@ export default {
   name: 'Login',
   data () {
     return {
-        user_id:"",
-        user_pwd:""
+        mail_address:"",
+        password:"",
+        errMailAddress:"false",
+        errMsgMailAddress:"",
+        errPassword:"false",
+        errMsgPassword:""
     }
   },
   methods:{
     loginSubmit: function() {
       console.log("로그인")
-      axios.post(`http://127.0.0.1:3000/login`, 
-        { 
-            email: this.user_id,
-            password: this.user_pwd
-        }).then(
-            (response) => { console.log(response) },
-	        (error) => { console.log(error) }
-            // res=> {
-            //     console.log(res)
-            //     this.$router.push("/MainContent")
-            // }
-        )
+       if (!this.chkValidation()) {
+         return false
+      } else {
+        axios.post(`http://127.0.0.1:3000/login`, 
+          { 
+              email: this.mail_address,
+              password: this.password
+          }).then(
+              (response) => { console.log(response) },
+            (error) => { console.log(error) }
+              // res=> {
+              //     console.log(res)
+              //     this.$router.push("/MainContent")
+              // }
+          )
+      }
     },
     moveToJoin: function() {
         this.$router.push("/Join")
     },
+    chkValidation: function() {
+        //메일 미입력 체크
+        if (this.mail_address == null || this.mail_address == "") {
+          this.errMailAddress = true
+          this.errMsgMailAddress = "메일주소를 입력해주세요."
+        } else {
+          this.errMailAddress = false
+          this.errMsgMailAddress = ''
+        }
+        //패스워드 미입력 체크
+        if (this.password == null || this.password == '') {
+          this.errPassword = true
+          this.errMsgPassword = "패스워드를 입력해주세요."
+        } else {
+          this.errPassword = false
+          this.errMsgPassword = ''
+        }
+        
+        //에러가 없을때만 true를 반환
+        if (this.errMailAddress || this.errPassword) {
+          return false
+        } else {
+          return true
+        }
+    }
   }
 }
 </script>
+<style>
+.invalid-feedback {
+  display: block;
+  color: #e50000;
+  font-size: 11px;
+  font-weight: bold;
+  width: 55%;
+  margin-left: 50%;
+}
+label {
+    float: left;
+    width: 55%;
+}
+</style>
