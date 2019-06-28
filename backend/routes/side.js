80a7e481698965b3f856f1631e1e6037870c161b
kimheegeon <32567051+kimheegeon@users.noreply.github.com>
2019-06-28 19:47:57 +0900

    side 수정

--- backend/routes/side.js
+++ backend/routes/side.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var mysql = require('mysql');
 var db = require('../db-config.json');
 
+
+
+var con = mysql.createConnection({
+  host: db.host,
+  user: db.user,
+  password: db.password,
+  database: db.database
+})
 
 
 router.get('/', function (req, res, next) {
     console.log("side 시작 !");
 
-    console.log(req.body.uid);
+    console.log(req.query.uid);
 
     var sql = "SELECT * FROM user WHERE UID = ?";
 
-    con.query(sql, [req.session.user.uid], function (error, result) {
+    con.query(sql, [req.query .uid], function (error, result) {
         if (error) throw error;
         else {
             res.json(
